d69eefe25892437c9268e3c73d3fb28c94e3d999
Komal-Prajapat <komalprajapat267@gmail.com>
2024-02-29 14:10:01 +0530

    Company Model , Customer Model , Employe Model

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ import bodyParser from "body-parser";
 import express  from "express";
 import mongoose from "mongoose";
 import { companyRouter } from "./router/companyRouter.js";
-import { UserRouter } from "./router/userRouter.js";
 import { CustomerRouter } from "./router/customerRouter.js";
-import { inventoryRouter } from "./router/inventoryRouter.js";
-import { salesRouter } from "./router/salesRouter.js";
 import { EmployeRouter } from "./router/EmployeRouter.js";
+// import { UserRouter } from "./router/userRouter.js";
+// import { inventoryRouter } from "./router/inventoryRouter.js";
+// import { salesRouter } from "./router/salesRouter.js";
+
 const app = express()
 
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.use("/api",companyRouter);
-app.use("/api",UserRouter);
+// app.use("/api",UserRouter);
 app.use("/api",CustomerRouter);
-app.use("/api",inventoryRouter);
-app.use("/api",salesRouter);
+// app.use("/api",inventoryRouter);
+// app.use("/api",salesRouter);
 app.use("/api",EmployeRouter)
 
 mongoose
@@ -25,7 +26,7 @@ mongoose
     dbName: "restaurant",
   })
   .then(() => console.log("MongoDB Connected..!"));
-
+    
 app.listen(9010 ,()=>{
     console.log("server is connected");
 })
